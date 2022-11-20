900245ce3af82f94691b94a64052b51a381f665b
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-11-20 08:49:02 +0100

    init with background

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LetsComposteIt.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
